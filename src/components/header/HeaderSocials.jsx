8e0cc13d767983440f717da994801df1d325d024
mchafiz <mchaerulhafiz@gmail.com>
2022-04-23 13:21:05 +0700

    define init css variable setup at index.css, update header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsLinkedin, BsGithub, BsDribbble } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
+      <a href="https://dribble.com" target="_blank" rel="noreferrer">
+        <BsDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
